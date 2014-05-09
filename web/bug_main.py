d03a43c546740a45318ffa4724afc408fa6e2661
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2014-05-09 16:23:38 -0700

    Fixed HTML and parses forms

--- web/bug_main.py
+++ web/bug_main.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
+#@app.route('/', methods=['POST', 'GET'])
+#def index():
+#    if request.method == 'POST':
+#        print "posting method used"
+#    elif request.method == 'GET':
+#        print "get method used"
+#    
+#    return render_template('bug_control.html')
+
 @app.route('/', methods=['POST', 'GET'])
-def index():
+@app.route('/<data>', methods=['POST', 'GET'])
+def boom(data=0):
     if request.method == 'POST':
-        print "post method used"
+        if "boom" in request.form:
+            print "The boom is", request.form["boom"]
+        
+        if "bug" in request.form:
+            print "The bug is", request.form["bug"]
+        print "post method used: ", request.form
     elif request.method == 'GET':
-        print "get method used"
+        print "get method used: ", data
     
     return render_template('bug_control.html')
 
@@ -14,3 +29,7 @@ if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port=80)
 
+def write_file(filename, data):
+    f = open(str(filename), "w")
+    f.write(str(data))
+    f.close()
