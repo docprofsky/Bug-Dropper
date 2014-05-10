d483b4dd596829d3ad8cc85c83e7224c31d9ce12
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2014-05-10 16:07:10 -0700

    Added exceptions need to fix bug in data processing

--- web/bug_main.py
+++ web/bug_main.py
@@ -11,6 +11,8 @@ UP = 1
 STOP = 0
 DOWN = -1
 
+positions = {"bug": STOP, "boom": UP}
+
 def write_file(filename, data):
     f = open(str(filename), "w")
     f.write(str(data))
@@ -18,25 +20,37 @@ def write_file(filename, data):
 
 def parse_data(formData):
     positions = dict()
+    data_submitted = list()
+
     if "boom" in formData:
-            if formData["boom"].lower() == "up":
-                positions["boom"] = UP
-            elif formData["boom"].lower() == "stop":
-                positions["boom"] = STOP
-        
+        if formData["boom"].lower() == "up":
+            positions["boom"] = UP
+            data_submitted.append("boom")
+        elif formData["boom"].lower() == "down":
+            positions["boom"] = STOP
+            data_submitted.append("boom")
+        else:
+            raise RuntimeError("Value for the boom position is not defined.")
+
     if "bug" in formData:
         if formData["bug"].lower() == "up":
             positions["bug"] = UP
+            data_submitted.append("bug")
         elif formData["bug"].lower() == "stop":
             positions["bug"] = STOP
+            data_submitted.append("bug")
         elif formData["bug"].lower() == "down":
             positions["bug"] = DOWN
+            data_submitted.append("bug")
+        else:
+            raise RuntimeError("Value for the bug position is not defined.")
 
-    return positions
+    return positions, data_submitted
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/<data>', methods=['POST', 'GET'])
 def boom(data=0):
+    global positions
     if request.method == 'POST':
         if "boom" in request.form:
             print "The boom is", request.form["boom"]
@@ -44,7 +58,14 @@ def boom(data=0):
         if "bug" in request.form:
             print "The bug is", request.form["bug"]
 
-        print parse_data(request.form)
+        new_position, data_submitted = parse_data(request.form)
+
+        for i in data_submitted:
+            if new_position[i] != positions[i]:
+                positions = new_position[i]
+
+        print new_position
+        print "positions :", positions
     
     return render_template('bug_control.html')
 
