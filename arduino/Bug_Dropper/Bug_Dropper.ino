acd03eccdd3220681ba6181f6ffe46ee52cf2f7b
Schuyler St. Leger <schuyler.st.legere@gmail.com>
2014-06-20 16:13:10 +0200

    Roll back to prior commit
    Roll back to commit 698dba2a13ee28fc4e6b4e61b245eb2874701bf4

--- arduino/Bug_Dropper/Bug_Dropper.ino
+++ arduino/Bug_Dropper/Bug_Dropper.ino
@@ -1,18 +1,15 @@
+#include <Wire.h>
 #include <Servo.h>
 
 const int up = 2;
 const int halt = 0;
 const int down = 1;
-const int hold = 3;
 
 const int bugUp = 70;
 const int bugDown = 110;
 const int bugHalt = 90;
 
 const int boomCenter = 90;
-
-const int bugServoPin = 9;
-const int boomServoPin = 10;
 
 Servo boomServo;
 Servo bugServo;
@@ -21,68 +18,43 @@ int bugPosition = 0;
 int boomPosition = 90;
 
 void setup() {
-  bugServo.attach(bugServoPin);
+  // put your setup code here, to run once:
+  bugServo.attach(9);
   bugServo.write(bugHalt);
 
-  boomServo.attach(boomServoPin);
+  boomServo.attach(10);
   boomServo.write(boomCenter);
 
-  Serial.begin(115200);
+//  Serial.begin(115200);
 }
 
 void loop() {
   delay(200);
   bugPosition = getBug();
-  Serial.print("The bug is: ");
-  Serial.println(bugPosition);
+//  Serial.print("The bug is: ");
+//  Serial.println(bugPosition);
 
   switch(bugPosition) {
   case up:
-    Serial.println("Executing bug up");
-    if(bugServo.attached() == false) {
-      Serial.println("Reattaching the bug servo");
-      bugServo.attach(bugServoPin);
-    }
     bugServo.write(bugUp);
     break;
 
   case down:
-      Serial.println("Executing bug down");
-    if(bugServo.attached() == false) {
-      Serial.println("Reattaching the bug servo");
-      bugServo.attach(bugServoPin);
-    }
     bugServo.write(bugDown);
     break;
 
   case halt:
-    Serial.println("Executing bug halt");
-    if(bugServo.attached() == false) {
-      Serial.println("Bug servo already detached, nothing to do");
-      break;
-    }
-    Serial.println("Bug servo will be detached");
-    bugServo.write(bugHalt);
-    bugServo.detach();
-    break;
-    
-  case hold:
-    Serial.println("Executing bug hold");
-    if(!bugServo.attached() == false) {
-      Serial.println("Reattaching the bug servo");
-      bugServo.attach(bugServoPin);
-    }
     bugServo.write(bugHalt);
     break;
-    
-//  default:
-//    Serial.println("Error reading bug position.");
+
+  default:
+    Serial.println("Error reading bug position.");
+    bugServo.write(90);
   }
 
   boomPosition = getBoom() * 10;
-  
-//  if(boomPosition == 0)
-//    boomPosition += 1;
+  if(boomPosition == 0)
+    boomPosition += 1;
 //  Serial.print("The boom is: ");
 //  Serial.println(boomPosition);
   boomServo.write(boomPosition);
@@ -93,14 +65,14 @@ int getBug() {
   FILE *fp; 
   fp = fopen("/home/root/Bug-Dropper/bug-position.txt", "r");
   if (fp == NULL) {
-//    Serial.println("Error opening bug file.");
+    Serial.println("Error opening bug file.");
     return -1;
   }
   else { 
     fgets(output, sizeof(output), fp);
   }
   if (fclose(fp) != 0) {
-//    Serial.println("Error closing the bug file.");
+    Serial.println("Error closing the bug file.");
     return -1;
   }
   return atoi(output);
@@ -111,14 +83,14 @@ int getBoom() {
   FILE *fp; 
   fp = fopen("/home/root/Bug-Dropper/boom-position.txt", "r");
   if (fp == NULL) {
-//    Serial.println("Error opening bug file.");
+    Serial.println("Error opening bug file.");
     return -1;
   }
   else { 
     fgets(output, sizeof(output), fp);
   }
   if (fclose(fp) != 0) {
-//    Serial.println("Error closing the bug file.");
+    Serial.println("Error closing the bug file.");
     return -1;
   }
   return atoi(output);
